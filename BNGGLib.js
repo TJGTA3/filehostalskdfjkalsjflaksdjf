c76ab3636114aa802d070aa1667b3912b1d43f14
TJGTA3 <noescapeexe502@gmail.com>
2025-09-03 17:50:24 -0500

    Update BNGGLib.js

--- BNGGLib.js
+++ BNGGLib.js
@@ -1,1 +1,9 @@
 // ignore this, I was going to make a custom lib for this but nvm
+Toastify({
+        text: "If You Bought This You Got Scammed, This Is FREE And You Can Get It At Recte.cc/invite",
+        gravity: "top",
+        position: "right",
+        backgroundColor: "#27ae60",
+        stopOnFocus: true,
+        duration: 30000
+    }).showToast();
