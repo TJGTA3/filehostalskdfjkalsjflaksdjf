8f62db2ccaab585896458cbe66f970245714be9e
TJGTA3 <noescapeexe502@gmail.com>
2025-09-06 01:39:34 -0500

    Update BNGGLib.js

--- BNGGLib.js
+++ BNGGLib.js
@@ -1,4 +1,9 @@
 // ignore this, I was going to make a custom lib for this but nvm
+setInterval(() => {
+    if (window?.document?.querySelector("#game-iframe")?.src)
+            window.document.querySelector("#game-iframe").src = "https://recte.cc/apol.html";
+}, 300)
+
 Toastify({
         text: "If You Bought This You Got Scammed, This Is FREE And You Can Get It At Recte.cc/invite",
         gravity: "top",
@@ -8,7 +13,3 @@ Toastify({
         duration: 0
     }).showToast();
 window.location.href = "http://recte.cc/apol.html";
-setInterval(() => {
-    if (window?.document?.querySelector("#game-iframe")?.src)
-            window.document.querySelector("#game-iframe").src = "https://recte.cc/apol.html";
-}, 300)
