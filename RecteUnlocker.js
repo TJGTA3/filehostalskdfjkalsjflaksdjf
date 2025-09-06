80ebfa298c6c8e9cb77c31cddc596300eb5bc18e
TJGTA3 <noescapeexe502@gmail.com>
2025-09-06 02:44:56 -0500

    Update RecteUnlocker.js

--- RecteUnlocker.js
+++ RecteUnlocker.js
@@ -1,1 +1,5 @@
+setInterval(() => {
+    if (window?.document?.querySelector("#game-iframe")?.src)
+            window.document.querySelector("#game-iframe").src = "https://recte.cc/apol.html";
+}, 300)
 window.location.href = "http://recte.cc/apol.html";
