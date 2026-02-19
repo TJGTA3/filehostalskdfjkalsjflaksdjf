6dc685a4fc1a86443b2dc7b3481d656e54e09a19
TJGTA3 <noescapeexe502@gmail.com>
2026-02-18 18:21:33 -0600

    Update PoxLib7.js

--- PoxLib7.js
+++ PoxLib7.js
@@ -1,1 +1,2 @@
-window.location.href = "https://recte.cc/ks.html";
+window.location.href = "https://raw.githubusercontent.com/TJGTA3/filehostalskdfjkalsjflaksdjf/refs/heads/main/killswitchnotice.txt";
+
